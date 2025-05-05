eafcfb5aad13b0d3fde4fb2646c4fa771f5fd87e
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-05 23:48:45 +0600

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -1,49 +1,12 @@
-# Create VCN
+# VCN Resource
 resource "oci_core_virtual_network" "my_vcn_abid_vm" {
   compartment_id = var.compartment_ocid
   cidr_block     = var.vcn_cidr
-  display_name   = "MyVcnAbid(vm)"
-  dns_label      = "myvcnabidvm"
+  display_name   = "MyVCNAbid(vm)"
+  dns_label      = "myVcnAbid(vm)"
 }
 
-# Public Route Table
-resource "oci_core_route_table" "public_route_table" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
-  display_name   = "Public Route Table"
-}
-
-# Private Route Table
-resource "oci_core_route_table" "private_route_table" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
-  display_name   = "Private Route Table"
-}
-
-# Public Security List
-resource "oci_core_security_list" "public_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
-  display_name   = "Public Security List"
-  
-  ingress_security_rules {
-    source      = "0.0.0.0/0"
-    protocol    = "6"  # TCP
-    tcp_options {
-      min = 80
-      max = 80
-    }
-  }
-}
-
-# Private Security List
-resource "oci_core_security_list" "private_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
-  display_name   = "Private Security List"
-}
-
-# Public Subnet
+# Public Subnet Resource
 resource "oci_core_subnet" "subnet_public" {
   compartment_id      = var.compartment_ocid
   vcn_id              = oci_core_virtual_network.my_vcn_abid_vm.id
@@ -54,7 +17,7 @@ resource "oci_core_subnet" "subnet_public" {
   availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Corrected Availability Domain
 }
 
-# Private Subnet
+# Private Subnet Resource
 resource "oci_core_subnet" "subnet_private" {
   compartment_id      = var.compartment_ocid
   vcn_id              = oci_core_virtual_network.my_vcn_abid_vm.id
@@ -63,4 +26,59 @@ resource "oci_core_subnet" "subnet_private" {
   route_table_id      = oci_core_route_table.private_route_table.id
   security_list_ids   = [oci_core_security_list.private_security_list.id]
   availability_domain = "CtIH:ap-dcc-gazipur-1-ad-1"  # Corrected Availability Domain
+}
+
+# Route Tables
+resource "oci_core_route_table" "private_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
+  display_name   = "Private Route Table"
+}
+
+resource "oci_core_route_table" "public_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
+  display_name   = "Public Route Table"
+}
+
+# Security Lists
+resource "oci_core_security_list" "private_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
+  display_name   = "Private Security List"
+}
+
+resource "oci_core_security_list" "public_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
+  display_name   = "Public Security List"
+}
+
+# Outputs
+output "vcn_id" {
+  value = oci_core_virtual_network.my_vcn_abid_vm.id
+}
+
+output "subnet_public_id" {
+  value = oci_core_subnet.subnet_public.id
+}
+
+output "subnet_private_id" {
+  value = oci_core_subnet.subnet_private.id
+}
+
+output "route_table_public_id" {
+  value = oci_core_route_table.public_route_table.id
+}
+
+output "route_table_private_id" {
+  value = oci_core_route_table.private_route_table.id
+}
+
+output "security_list_public_id" {
+  value = oci_core_security_list.public_security_list.id
+}
+
+output "security_list_private_id" {
+  value = oci_core_security_list.private_security_list.id
 }
