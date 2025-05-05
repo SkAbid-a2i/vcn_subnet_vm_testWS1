60ae28a7be70ed3231e9cbd51b077f6936cee579
Sk. Al-Abid <sk.abid@a2i.gov.bd>
2025-05-05 23:54:57 +0600

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -53,28 +53,3 @@ resource "oci_core_security_list" "public_security_list" {
   vcn_id         = oci_core_virtual_network.my_vcn_abid_vm.id
   display_name   = "Public Security List"
 }
-
-
-output "subnet_public_id" {
-  value = oci_core_subnet.subnet_public.id
-}
-
-output "subnet_private_id" {
-  value = oci_core_subnet.subnet_private.id
-}
-
-output "route_table_public_id" {
-  value = oci_core_route_table.public_route_table.id
-}
-
-output "route_table_private_id" {
-  value = oci_core_route_table.private_route_table.id
-}
-
-output "security_list_public_id" {
-  value = oci_core_security_list.public_security_list.id
-}
-
-output "security_list_private_id" {
-  value = oci_core_security_list.private_security_list.id
-}
